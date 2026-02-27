f8ddb85e094bb019983bccd02d801b6c68c6ec92
Eason <easondonner@gmail.com>
2026-02-26 21:28:38 -0800

    changed coinToss() to a formatted string.

--- iChing.js
+++ iChing.js
@@ -1,4 +1,19 @@
 
+
+var askDiv = document.getElementById("questionSection");
+
+//hide the tossing div until the question is asked
+var tossDiv = document.getElementById("throwingSection");
+tossDiv.style.display = "none";
+
+//show the tossing div and hide the asking div when the question is asked
+const questionForm = document.querySelector("questionForm");
+questionForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    askDiv.style.display = "none";
+    tossDiv.style.display = "block";
+});
+
 // return a random boolean value, with a 30% chance of being true and a 70% chance of being false
 function randomBoolean() {
     if(Math.random() < .3) {
@@ -9,19 +24,22 @@ function randomBoolean() {
 } 
 // simulate tossing three coins and return the corresponding I Ching line type
 function coinToss() {
-    let c1 = randomBoolean();
-    let c2 = randomBoolean();
-    let c3 = randomBoolean();
-    let csum = c1 + c2 + c3;
+    let threeCoins = [];
+    for(let i= 0; i<3; i++) {
+        threeCoins.push(randomBoolean()); 
+        }
+    let csum = threeCoins.reduce((a, b) => a + b, 0);
+    let lName;
     if(csum === 6) {
-        return "Changing Yin";
+        lName = "Changing Yin";
     } else if(csum === 7) {
-        return "Stable Yang";
+        lName = "Stable Yang";
     } else if(csum === 8) {
-        return "Stable Yin";
+        lName = "Stable Yin";
     } else if(csum === 9) {
-        return "Changing Yang";
+        lName = "Changing Yang";
     }
+    return `${lName} (${threeCoins[0]}+${threeCoins[1]}+${threeCoins[2]}=${csum})`;
 }
-toss1 = coinToss()
-console.log(toss1);
+
+console.log(coinToss());
